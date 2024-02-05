fb49ea1250ed3dd8bf8c2defe8f97fe6e0e9095b
AtsushiYanaigsawa768 <yanagisawa.atsushi.62t@st.kyoto-u.ac.jp>
2024-02-05 18:51:29 +0900

    所属機関の表記方法を修正

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -12,13 +12,13 @@ export default function organization({ index, value }: PageProps) {return (
       The organizing team consists of experts and students from various institution.
       </Paragraph>
       <ul style={{ listStyle: 'circle', paddingLeft: '0' }}>
-      <li>Shiyao Ding:Kyoto University,Assistant professor,<a href="https://www.dingshiyao.com/">HP</a></li>
-      <li>Hideki Aoyama:Panasonic Holdings Corporation,<a href="https://hidekia.github.io/">HP</a></li>
-      <li>Donghui Lin:Okayama University,Associate Professor,<a href="https://lindh.github.io/">HP</a></li>
-      <li>Fumito Uwano:Okayama University,Assistant Professor,<a href="https://soran.cc.okayama-u.ac.jp/html/24d9a3e377f2d39a74506e4da22f6611_en.html">HP</a></li><br />
-      <li>Ryo Magoshi:Kyoto University,student</li>
-      <li>Lee Kunwoo:Kyoto University,student</li>
-      <li>Atsushi Yanagisawa:Kyoto University,student </li>
+      <li>Shiyao Ding(Kyoto University,<a href="https://www.dingshiyao.com/">HP</a>)</li>
+      <li>Hideki Aoyama(Panasonic Holdings Corporation,<a href="https://hidekia.github.io/">HP</a>)</li>
+      <li>Donghui Lin(Okayama University,<a href="https://lindh.github.io/">HP</a>)</li>
+      <li>Fumito Uwano(Okayama University,<a href="https://soran.cc.okayama-u.ac.jp/html/24d9a3e377f2d39a74506e4da22f6611_en.html">HP</a>)</li><br />
+      <li>Ryo Magoshi(Kyoto University)</li>
+      <li>Lee Kunwoo(Kyoto University)</li>
+      <li>Atsushi Yanagisawa(Kyoto University)</li>
       </ul>
     </Content>
     );
