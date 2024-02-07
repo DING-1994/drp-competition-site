13fa5ff8c381412de15cf55f4e75e31b096de1c1
yukihira-pot <nagohachi.study@gmail.com>
2024-02-07 15:30:30 +0900

    ログアウトボタンを追加

--- src/components/appbar/Header.tsx
+++ src/components/appbar/Header.tsx
@@ -15,6 +15,7 @@ import SignUpForm from "../form/SignUpForm";
 import SignInForm from "../form/SignInForm";
 import { UserInfo } from "../../hooks/useUserInfo";
 import { FormInfo } from "../../hooks/useFormInfo";
+import { logout } from "../../lib/firebase/auth";
 
 interface HeaderProps {
   setTabValue: React.Dispatch<React.SetStateAction<TAB_VALUES_TYPE>>;
@@ -58,6 +59,14 @@ export default function Header({
           // }}
         >
           Sign in
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ margin: "1rem", textTransform: "none", fontWeight: "bold" }}
+          onClick={() => logout()}
+        >
+          Sign out
         </Button>
         <Button
           variant="contained"
