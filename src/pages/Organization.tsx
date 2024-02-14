da83bae33c0b7d747cc052d74eb4a5875b3bf998
yukihira-pot <nagohachi.study@gmail.com>
2024-02-14 16:13:09 +0900

    Organization ページを修正

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -3,22 +3,38 @@ import { Paragraph } from "../components/paragraphComponents/Paragraph";
 import { PageProps } from "./props/PageProps";
 import Content from "../components/body/Content";
 
-export default function organization({ index, value }: PageProps) {
+export default function Organization({ index, value }: PageProps) {
   return (
     <Content index={index} value={value}>
-      <HeadingWithLink id="organizers">
-      Organizers
-      </HeadingWithLink>
+      <HeadingWithLink id="organizers">Organizers</HeadingWithLink>
       <Paragraph>
-      The organizing team consists of experts and students from various institution.
+        The organizing team consists of experts and students from various
+        institutions.
       </Paragraph>
-      <li>Shiyao Ding ( Kyoto University, <a href="https://www.dingshiyao.com/">HP</a> ) </li>
-      <li>Hideki Aoyama ( Panasonic Holdings Corporation, <a href="https://hidekia.github.io/">HP</a> ) </li>
-      <li>Donghui Lin ( Okayama University, <a href="https://lindh.github.io/">HP</a> ) </li>
-      <li>Fumito Uwano ( Okayama University, <a href="https://soran.cc.okayama-u.ac.jp/html/24d9a3e377f2d39a74506e4da22f6611_en.html">HP</a> ) </li><br />
-      <li>Ryo Magoshi ( Kyoto University ) </li>
-      <li>Lee Kunwoo ( Kyoto University ) </li>
-      <li>Atsushi Yanagisawa ( Kyoto University ) </li>
+      <ul>
+        <li>
+          Shiyao Ding (Kyoto University,{" "}
+          <a href="https://www.dingshiyao.com/">HP</a>)
+        </li>
+        <li>
+          Hideki Aoyama (Panasonic Holdings Corporation,{" "}
+          <a href="https://hidekia.github.io/">HP</a>)
+        </li>
+        <li>
+          Donghui Lin (Okayama University,{" "}
+          <a href="https://lindh.github.io/">HP</a>)
+        </li>
+        <li>
+          Fumito Uwano (Okayama University,{" "}
+          <a href="https://soran.cc.okayama-u.ac.jp/html/24d9a3e377f2d39a74506e4da22f6611_en.html">
+            HP
+          </a>
+          )
+        </li>
+        <li>Ryo Magoshi (Kyoto University)</li>
+        <li>Lee Kunwoo (Kyoto University)</li>
+        <li>Atsushi Yanagisawa (Kyoto University)</li>
+      </ul>
     </Content>
-    );
+  );
 }
