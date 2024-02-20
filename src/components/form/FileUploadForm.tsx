2c5d835005cbcc94bb5f39eb65b2e40d4e2ceb07
yukihira-pot <nagohachi.study@gmail.com>
2024-02-20 15:22:30 +0900

    スタイルを変更、バリデーションが通った時のみアップロードできるように修正

--- src/components/form/FileUploadForm.tsx
+++ src/components/form/FileUploadForm.tsx
@@ -1,25 +1,27 @@
+import { Button } from "@mui/material";
+import { ref, uploadBytes } from "firebase/storage";
 import { useState } from "react";
 import { storage } from "../../lib/firebase/firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import styles from "../../styles/FileUploadForm.module.css";
 
 export default function FileUploadForm() {
   const [file, setFile] = useState<File | null>(null);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const [inputDisabled, setInputDisabled] = useState(false);
 
-  const validateFile = (file: File) => {
-    // .py ファイルのみ受け付ける
-    if (file.name.split(".").pop() !== "py") {
-      return false;
-    }
-    return true;
-  };
+  const validateFile = (file: File) => file.name.split(".").pop() === "zip";
 
   const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
-      const file = event.target.files[0];
+      const file = event.target.files[event.target.files.length - 1];
       if (validateFile(file)) {
         setFile(file);
+        setSubmitDisabled(false);
+        setInputDisabled(true);
       } else {
-        alert("Please upload a .py file.");
+        alert("Please upload a .zip file.");
+        setSubmitDisabled(true);
+        setInputDisabled(false);
       }
     }
   };
@@ -38,8 +40,25 @@ export default function FileUploadForm() {
   return (
     <>
       <form onSubmit={uploadFile}>
-        <input type="file" onChange={handleFileSelect} />
-        <button type="submit">Upload</button>
+        <div className={styles.file_upload}>
+          <input
+            type="file"
+            id="file"
+            className={styles.file_input}
+            onChange={handleFileSelect}
+          />
+          <label
+            htmlFor="file"
+            className={
+              inputDisabled ? styles.file_label_disabled : styles.file_label
+            }
+          >
+            Choose a file
+          </label>
+        </div>
+        <Button type="submit" variant="contained" disabled={submitDisabled}>
+          Upload
+        </Button>
       </form>
       {file && <p>Selected file: {file.name}</p>}
     </>
