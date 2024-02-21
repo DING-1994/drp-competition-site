7a5f60f62395e6be2a58555e79cd74cfc7683380
yukihira-pot <nagohachi.study@gmail.com>
2024-02-21 15:01:52 +0900

    Submission ページに、ファイルアップロード成功時に使用する Snackbar を追加

--- src/pages/Submissions.tsx
+++ src/pages/Submissions.tsx
@@ -2,11 +2,15 @@ import Content from "../components/body/Content";
 import FileUploadForm from "../components/form/FileUploadForm";
 import HeadingWithLink from "../components/paragraphComponents/HeadingWithLink";
 import { Paragraph } from "../components/paragraphComponents/Paragraph";
+import SuccessSnackbar from "../components/snackbar/SuccessSnackbar";
+import { useFormInfo } from "../hooks/useFormInfo";
 import { useAuthContext } from "../lib/context/AuthContext";
 import { PageProps } from "./props/PageProps";
 
 export default function Submissions({ index, value }: PageProps) {
   const { currentUser } = useAuthContext();
+  const formInfoState = useFormInfo();
+
   return (
     <Content index={index} value={value}>
       <HeadingWithLink id="submissions">Submissions</HeadingWithLink>
@@ -19,11 +23,16 @@ export default function Submissions({ index, value }: PageProps) {
               {currentUser.displayName} ({currentUser.email})
             </strong>
           </Paragraph>
-          <FileUploadForm />
+          <FileUploadForm formInfoState={formInfoState} />
         </>
       ) : (
         <Paragraph>You are not signed in.</Paragraph>
       )}
+      <SuccessSnackbar
+        open={formInfoState.formSentSuccess}
+        setFormSentSuccess={formInfoState.setFormSentSuccess}
+        message="Your file has been successfully uploaded."
+      />
     </Content>
   );
 }
