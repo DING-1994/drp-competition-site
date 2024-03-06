a567b6c2136798465d152295273e7ff8008cae11
Shiyao DING <dingshiyao0217@gmail.com>
2024-03-06 10:37:17 +0900

    Update RulesAndGuidelines.tsx

--- src/pages/RulesAndGuidelines.tsx
+++ src/pages/RulesAndGuidelines.tsx
@@ -9,55 +9,27 @@ export default function RulesAndGuidelines({ index, value }: PageProps) {
   return (
     <Content index={index} value={value}>
       <HeadingWithLink id="registration-and-participation">
-        Registration and Participation
+        Participation
       </HeadingWithLink>
       <Paragraph>
-        To participate in this competition, teams must complete the registration
-        form accessed via the "Participate" button at the top of the page.
-      </Paragraph>
-      <Paragraph>
-        You can participate in this Challenge alone or as a team. There is no
-        maximum team size, and the minimum team size is one person. Participants
-        may not collaborate unless those participants entered this Challenge as
-        a team; participants who are members of the same team may collaborate
-        with one another. Teams may not be merged.
+       To participate in this competition, please complete the registration by clicking the "Participate" button
+        at the top of the page. Participants can enter the competition either individually or 
+        as part of a team. When registering as a team, please ensure all members use the same team name. 
+        There is no limit to the number of members per team.
       </Paragraph>
       <HeadingWithLink id="submission">Submission</HeadingWithLink>
       <Paragraph>
-        Conditioned upon compliance with these rules, the Organizers will award
-        Prizes to the participants with the highest scores, which are evaluated
-        by the competition evaluation criteria ( defined below ). Participants
-        need to submit these four things through Google form during the
-        submission period.
+        After completing registration, participants can begin to develop their contributions. 
+        You are required to submit a zip file containing the following components:
       </Paragraph>
-
-      <HeadingWithLink id="q-and-a">Q&A</HeadingWithLink>
-      <Paragraph>
-        To support participants and ensure the success of this competition, we
-        plan to accept questions from participants. A{" "}
-        <a href="https://github.com/DrpChallenge/main">GitHub repository</a>{" "}
-        will be set up to handle participant questions. The content of the Q&A
-        session will be made publicly available on the homepage to ensure all
-        participants have access to the information unless the question is
-        related to personal information. Additionally, there are plans to
-        include instructions on how to use the environment and provide a basic
-        guide on using Stable Baselines.
-      </Paragraph>
-
       <Ul>
         <li>
-          Source code: Please provide all the code used to generate these
-          results. Additionally, include moderately concise comments to ensure
-          that others can understand the code when reading it. It is strictly
-          prohibited to reuse code from other teams.
+         Source Code: Please include all the code used to generate your results.
         </li>
         <li>
-          Academic report ( optional ) : Brief instruction of the method, one
-          page ( excluding reference ) , should be written in English. Whether
-          to submit the academic report is optional. If there is any related
-          information used to derive the results, this information must be
-          contained in their academic report.
+          Academic Report: A brief description of your method, to be provided in English. 
         </li>
+      You may submit your contributions multiple times; however, only the final submission will be considered for evaluation.
       </Ul>
 
       <HeadingWithLink id="finalist">Finalist</HeadingWithLink>
@@ -100,6 +72,10 @@ export default function RulesAndGuidelines({ index, value }: PageProps) {
         agents and the environment. For instance, modifying the definition of
         collisions or extending the range of agent movement beyond nodes and
         edges is strictly prohibited.
+      </Paragraph>
+            <HeadingWithLink id="q-and-a">Q&A</HeadingWithLink>
+      <Paragraph>
+       Please　be free to consult with drpcommon2023@gmail.com, if you have any question
       </Paragraph>
     </Content>
   );
