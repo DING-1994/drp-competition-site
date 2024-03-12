7bc708e1ca74a8e91dbf7be110982336102a3b13
yukihira-pot <nagohachi.study@gmail.com>
2024-03-12 16:10:16 +0900

    タブを修正

--- src/components/appbar/HeaderTabs.tsx
+++ src/components/appbar/HeaderTabs.tsx
@@ -1,7 +1,16 @@
 import { TAB_VALUES, TAB_VALUES_TYPE } from "../../App";
-import { Tab, Tabs } from "@mui/material";
+import {
+  Divider,
+  Drawer,
+  IconButton,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Box } from "@mui/system";
 
 interface HeaderTabsProps {
   setTabValue: React.Dispatch<React.SetStateAction<TAB_VALUES_TYPE>>;
@@ -10,6 +19,12 @@ interface HeaderTabsProps {
 export default function HeaderTabs({ setTabValue }: HeaderTabsProps) {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
 
   const currentTabValue = (() => {
     const path = location.pathname.replace("#", "");
@@ -40,56 +55,151 @@ export default function HeaderTabs({ setTabValue }: HeaderTabsProps) {
       }
       navigate(`#${hash}`);
     }, 0);
+    console.log("currentTabValue: ", currentTabValue);
   }, []);
 
   const handleTabChange = (
     _: React.SyntheticEvent,
     newValue: TAB_VALUES_TYPE
   ) => {
-    setTabValue(newValue);
+    setTabValue(() => newValue);
   };
 
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Menu
+      </Typography>
+      <Divider />
+      <Tabs
+        onChange={handleTabChange}
+        orientation="vertical"
+        value={currentTabValue}
+        sx={{
+          alignSelf: "stretch",
+          paddingTop: "5vh",
+          display: { xs: "block", sm: "block", md: "none" },
+        }}
+      >
+        <Tab
+          label="Overview"
+          component={Link}
+          to="overview/"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Guidelines"
+          component={Link}
+          to="guidelines/"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Organization"
+          component={Link}
+          to="organization/"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Submissions"
+          component={Link}
+          to="submissions/"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Leaderboard"
+          component={Link}
+          to="leaderboard/"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+      </Tabs>
+    </Box>
+  );
+
+  const container = document.body;
+
   return (
-    <Tabs
-      value={currentTabValue}
-      onChange={handleTabChange}
-      sx={{ alignSelf: "stretch", paddingTop: "5vh" }}
-    >
-      <Tab
-        label="Overview"
-        component={Link}
-        to="/overview"
-        disableRipple
-        sx={{ textTransform: "none", fontWeight: "bold" }}
-      />
-      <Tab
-        label="Guidelines"
-        component={Link}
-        to="/guidelines"
-        disableRipple
-        sx={{ textTransform: "none", fontWeight: "bold" }}
-      />
-      <Tab
-        label="Organization"
-        component={Link}
-        to="/organization"
-        disableRipple
-        sx={{ textTransform: "none", fontWeight: "bold" }}
-      />
-      <Tab
-        label="Submissions"
-        component={Link}
-        to="/submissions"
-        disableRipple
-        sx={{ textTransform: "none", fontWeight: "bold" }}
-      />
-      <Tab
-        label="Leaderboard"
-        component={Link}
-        to="/leaderboard"
-        disableRipple
-        sx={{ textTransform: "none", fontWeight: "bold" }}
-      />
-    </Tabs>
+    <>
+      <IconButton
+        aria-label="open drawer"
+        edge="start"
+        onClick={handleDrawerToggle}
+        sx={{ mr: 2, display: { md: "none" }, padding: 0 }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Tabs
+        value={currentTabValue}
+        onChange={handleTabChange}
+        sx={{
+          alignSelf: "stretch",
+          paddingTop: "5vh",
+          display: { xs: "none", sm: "none", md: "block" },
+        }}
+      >
+        <Tab
+          label="Overview"
+          component={Link}
+          to="/overview"
+          disableRipple
+          sx={{
+            textTransform: "none",
+            fontWeight: "bold",
+          }}
+        />
+        <Tab
+          label="Guidelines"
+          component={Link}
+          to="/guidelines"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Organization"
+          component={Link}
+          to="/organization"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Submissions"
+          component={Link}
+          to="/submissions"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+        <Tab
+          label="Leaderboard"
+          component={Link}
+          to="/leaderboard"
+          disableRipple
+          sx={{ textTransform: "none", fontWeight: "bold" }}
+        />
+      </Tabs>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          // ModalProps={{
+          //   keepMounted: true,
+          // }}
+          sx={{
+            display: { sm: "block", md: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: "300px",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </>
   );
 }
