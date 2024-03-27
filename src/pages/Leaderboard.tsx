421edee72a45c19ae10d5aa343162c66d40dd113
yukihira-pot <nagohachi.study@gmail.com>
2024-03-27 14:56:48 +0900

    見出しを削除

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,5 +1,4 @@
 import Content from "../components/body/Content";
-import HeadingWithLink from "../components/paragraphComponents/HeadingWithLink";
 import ScoreTable from "../components/scoreTable/ScoreTable";
 import { PageProps } from "./props/PageProps";
 
@@ -7,7 +6,6 @@ export default function Leaderboard({ index, value }: PageProps) {
   // const { currentUser } = useAuthContext();
   return (
     <Content index={index} value={value}>
-      <HeadingWithLink id="leaderboard">Leaderboard</HeadingWithLink>
       <ScoreTable />
     </Content>
   );
